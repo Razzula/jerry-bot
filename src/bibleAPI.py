10b21caf351fc427111e66a2c7cb77394df43f6d
Razzula <jack02gillespie@gmail.com>
2024-02-23 20:59:38 +0100

    Initial refacotring of bot method

--- src/bibleAPI.py
+++ src/bibleAPI.py
@@ -6,16 +6,19 @@ from bs4 import BeautifulSoup, Tag
 
 
 try:
-    with open('data/static/usfms.json', 'r') as file:
+    with open("data/static/usfms.json", "r") as file:
         usfmManifest = json.load(file)
 except:
     print("Error loading USFM manifest")
     usfmManifest = []
 
 
-def getUSFMsFromString(reference: str, current_book = None, current_chapter = None):
-    
-    match = re.search(r'(I+|[123]+)?\s*([A-Za-z]+)\.? *(\d+)(?::\s*(\d+)(?:\s*-\s*(\d+))?|-(\d+))?(?:.*?([;,].*))?', reference.upper())
+def getUSFMsFromString(reference: str, current_book=None, current_chapter=None):
+
+    match = re.search(
+        r"(I+|[123]+)?\s*([A-Za-z]+)\.? *(\d+)(?::\s*(\d+)(?:\s*-\s*(\d+))?|-(\d+))?(?:.*?([;,].*))?",
+        reference.upper(),
+    )
     """
     1: book number (optional)               III     
     2: book name                            John    Genesis
@@ -30,73 +33,81 @@ def getUSFMsFromString(reference: str, current_book = None, current_chapter = No
 
     usfm = {}
 
-    if (match):
-        if ((match.group(2) == 'V') or (match.group(2) == 'VERSE')):  # VERSE REFERENCE
-            if ((current_book is None) or (current_chapter is None)):  # insufficient context
+    if match:
+        if (match.group(2) == "V") or (match.group(2) == "VERSE"):  # VERSE REFERENCE
+            if (current_book is None) or (
+                current_chapter is None
+            ):  # insufficient context
                 return []
-            
-            usfm['book'] = current_book
-            usfm['initialChapter'] = current_chapter
-            usfm['initialVerse'] = int(match.group(3))
-            usfm['finalVerse'] = int(match.group(6)) if match.group(6) else None
+
+            usfm["book"] = current_book
+            usfm["initialChapter"] = current_chapter
+            usfm["initialVerse"] = int(match.group(3))
+            usfm["finalVerse"] = int(match.group(6)) if match.group(6) else None
         else:
-            if ((match.group(2) == 'CH') or (match.group(2) == 'CHAPTER')):  # CHAPTER REFERENCE
-                if (current_book is None):  # insufficient context
+            if (match.group(2) == "CH") or (
+                match.group(2) == "CHAPTER"
+            ):  # CHAPTER REFERENCE
+                if current_book is None:  # insufficient context
                     return []
-                
-                usfm['book'] = current_book
+
+                usfm["book"] = current_book
             else:  # FULL REFERENCE
                 # book name
-                if (match.group(1) is None):
-                    match_group_1 = ''
+                if match.group(1) is None:
+                    match_group_1 = ""
                 else:
                     # account for Roman numerals
-                    if (match.group(1).startswith('I')):
+                    if match.group(1).startswith("I"):
                         match_group_1 = str(len(match.group(1)))
                     else:
                         match_group_1 = match.group(1)
-                
+
                 book_name = match_group_1 + match.group(2)
 
                 for book in usfmManifest:
-                    if (book_name in book):
-                        usfm['book'] = book[0]
+                    if book_name in book:
+                        usfm["book"] = book[0]
                         break
 
             # chapters, verses
-            usfm['initialChapter'] = int(match.group(3))
-            usfm['initialVerse'] = int(match.group(4)) if match.group(4) else None
-            usfm['finalVerse'] = int(match.group(5)) if match.group(5) else None
-            usfm['finalChapter'] = int(match.group(6)) if match.group(6) else None
+            usfm["initialChapter"] = int(match.group(3))
+            usfm["initialVerse"] = int(match.group(4)) if match.group(4) else None
+            usfm["finalVerse"] = int(match.group(5)) if match.group(5) else None
+            usfm["finalChapter"] = int(match.group(6)) if match.group(6) else None
 
     else:  # invalid format (may be shorthand reference)
-        if (current_book is None):  # insufficient context
+        if current_book is None:  # insufficient context
             return []
-        
-        match = re.search(r'(?:(\d+):)?(\d+)(?:-(\d+))?', reference.upper())
-        if (not match):  # invalid format
+
+        match = re.search(r"(?:(\d+):)?(\d+)(?:-(\d+))?", reference.upper())
+        if not match:  # invalid format
             return []
-        
-        usfm['book'] = current_book
+
+        usfm["book"] = current_book
 
         # chapters, verses
-        usfm['initialChapter'] = int(match.group(1)) if match.group(1) else current_chapter
-        usfm['initialVerse'] = int(match.group(2)) if match.group(2) else None
-        usfm['finalVerse'] = int(match.group(3)) if match.group(3) else None
+        usfm["initialChapter"] = (
+            int(match.group(1)) if match.group(1) else current_chapter
+        )
+        usfm["initialVerse"] = int(match.group(2)) if match.group(2) else None
+        usfm["finalVerse"] = int(match.group(3)) if match.group(3) else None
 
     return usfm
-    
+
 
 def extractReferences(text, currentBook=None, currentChapter=None):
-    if text == '':
+    if text == "":
         return []
     text = text.upper()
 
     # detect references
-    pattern = re.compile(r"(?:(?:(?:I+|[123]+?)?\s*)(?:[A-Za-z]+)\.? *|(?<=[;,])) ?\d+(?::\s*\d+(?:\s*-\s*\d+)?|-\d+)?")
+    pattern = re.compile(
+        r"(?:(?:(?:I+|[123]+?)?\s*)(?:[A-Za-z]+)\.? *|(?<=[;,])) ?\d+(?::\s*\d+(?:\s*-\s*\d+)?|-\d+)?"
+    )
     matches = re.findall(pattern, text)
 
-    if (not matches):
+    if not matches:
         return []
 
     references = []
@@ -104,18 +115,18 @@ def extractReferences(text, currentBook=None, currentChapter=None):
     for match in matches:
         usfm = getUSFMsFromString(match, currentBook, currentChapter)
 
-        if ((usfm.get("book") is None) or (usfm.get("initialChapter") is None)):
+        if (usfm.get("book") is None) or (usfm.get("initialChapter") is None):
             continue
 
         currentBook = usfm.get("book")
         currentChapter = usfm.get("initialChapter")
 
         reference = f'{usfm.get("book")} {usfm.get("initialChapter")}'
-        if (usfm.get("initialVerse") is not None):
+        if usfm.get("initialVerse") is not None:
             reference += f':{usfm.get("initialVerse")}'
-        if (usfm.get("finalVerse") is not None):
+        if usfm.get("finalVerse") is not None:
             reference += f'-{usfm.get("finalVerse")}'
-        if (usfm.get("finalChapter") is not None):
+        if usfm.get("finalChapter") is not None:
             reference += f'-{usfm.get("finalChapter")}'
 
         references.append(reference)
@@ -128,27 +139,27 @@ def getBibleReferences(text):
     text = text.upper()
 
     references = extractReferences(text)
-    if (references == []):
+    if references == []:
         return None
-    
-    encoodedVersion = urllib.parse.quote('NKJV')
-    for translation in ['NKJV','KJV', 'ESV', 'NIV', 'NLT']:
-        if (translation in text):
+
+    encoodedVersion = urllib.parse.quote("NKJV")
+    for translation in ["NKJV", "KJV", "ESV", "NIV", "NLT"]:
+        if translation in text:
             encoodedVersion = urllib.parse.quote(translation)
             continue
-    
+
     results = []
     for reference in references:
 
         encodedQuery = urllib.parse.quote(reference)
 
-        url = f'https://www.biblegateway.com/passage?search={encodedQuery}&version={encoodedVersion}'
+        url = f"https://www.biblegateway.com/passage?search={encodedQuery}&version={encoodedVersion}"
 
         try:
             response = requests.get(url)
 
             if response.status_code == 200:
-                
+
                 soup = BeautifulSoup(response.text, "html.parser")
 
                 verseElement = soup.select_one(".bcv")
@@ -165,17 +176,17 @@ def getBibleReferences(text):
                     for child in element.children:
 
                         # extract / format
-                        if (isinstance(child, Tag)):
-                            if (child.has_attr("class")):
+                        if isinstance(child, Tag):
+                            if child.has_attr("class"):
                                 # VERSE NUMBERS
-                                if ("versenum" in child["class"]):
-                                    text = f'**{child.get_text()}**'
+                                if "versenum" in child["class"]:
+                                    text = f"**{child.get_text()}**"
                                 # CHAPTER NUMBERS
-                                elif ("chapternum" in child["class"]):
-                                    text = f'**{1}**'
+                                elif "chapternum" in child["class"]:
+                                    text = f"**{1}**"
                                 # HEADINGS
-                                elif ("heading" in child["class"]):
-                                    text = f'__**{child.get_text()}**__'
+                                elif "heading" in child["class"]:
+                                    text = f"__**{child.get_text()}**__"
                                 else:
                                     text = child.get_text()
                             else:
@@ -188,22 +199,26 @@ def getBibleReferences(text):
 
                 # further formatting
                 content = " ".join(content)
-                content = re.sub(r'(\(|\[)[a-zA-Z]+(\)|\])', '', content) # exclude footnotes
-                content = re.sub(r'\s\s+', ' ', content) # remove extra spaces
+                content = re.sub(
+                    r"(\(|\[)[a-zA-Z]+(\)|\])", "", content
+                )  # exclude footnotes
+                content = re.sub(r"\s\s+", " ", content)  # remove extra spaces
 
                 contents = []
-                while (len(content) >= 1950):
+                while len(content) >= 1950:
                     i = 1950
-                    while (content[i] != ' '):
+                    while content[i] != " ":
                         i -= 1
-                    contents.append(content[:i+1])
-                    content = content[i+1:]
+                    contents.append(content[: i + 1])
+                    content = content[i + 1 :]
                 contents.append(content)
 
-                results.append([f'**{verse}** ({encoodedVersion})', contents])
+                results.append([f"**{verse}** ({encoodedVersion})", contents])
 
             else:
-                print(f"Request failed with status code: {response.status_code} - {response.reason}")
+                print(
+                    f"Request failed with status code: {response.status_code} - {response.reason}"
+                )
 
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
--- src/steamAPI.py
+++ src/steamAPI.py
@@ -3,16 +3,16 @@ import random
 import requests
 from dotenv import load_dotenv
 
-load_dotenv('token.env')
-key = os.environ.get('STEAM_API_KEY')
+load_dotenv("token.env")
+key = os.environ.get("STEAM_API_KEY")
 
 
 def GET(endpoint):
     response = requests.get(endpoint)
     if not response:
-        print('Error: ' + str(response.status_code))
+        print("Error: " + str(response.status_code))
 
-    if (response.status_code == 429): # too many requests
+    if response.status_code == 429:  # too many requests
         raise Exception("429")
 
     return response
@@ -25,36 +25,48 @@ def getSharedLibrary(users):
     for user in users:
         #  GET
         ## Profile Status
-        response = GET('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key='+key+'&steamids='+user+'&format=json')
-        if (response.status_code == 404): # not found
+        response = GET(
+            "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key="
+            + key
+            + "&steamids="
+            + user
+            + "&format=json"
+        )
+        if response.status_code == 404:  # not found
             continue
 
         result = response.json()
 
-        if (len(result['response']['players']) == 0):
-            print(f'user ({user}) not found')
+        if len(result["response"]["players"]) == 0:
+            print(f"user ({user}) not found")
             continue
-        if (result['response']['players'][0]['communityvisibilitystate'] < 3):
-            print(result['response']['players'][0]['personaname'] + 'is not public')
+        if result["response"]["players"][0]["communityvisibilitystate"] < 3:
+            print(result["response"]["players"][0]["personaname"] + "is not public")
             continue
 
         ## Games
-        response = GET('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key='+key+'&steamid='+user+'&format=json')
-        if (response.status_code == 404): # not found
+        response = GET(
+            "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key="
+            + key
+            + "&steamid="
+            + user
+            + "&format=json"
+        )
+        if response.status_code == 404:  # not found
             continue
 
         result = response.json()
 
         temp = []
-        if (games == [None]): # first sweep
-            for game in result['response']['games']:
-                temp.append(game['appid'])
+        if games == [None]:  # first sweep
+            for game in result["response"]["games"]:
+                temp.append(game["appid"])
 
         else:
-            for game in result['response']['games']:
-                if (game['appid'] in games):
-                    temp.append(game['appid'])
-            if (games == []):
+            for game in result["response"]["games"]:
+                if game["appid"] in games:
+                    temp.append(game["appid"])
+            if games == []:
                 break
 
         games = temp.copy()
@@ -65,49 +77,51 @@ def getSharedLibrary(users):
 def getGame(games, multiplayer):
     # SELECT GAME FROM LIST
 
-    while (True):
+    while True:
         id = random.choice(games)
         print(id)
 
         ## App Data
-        response = GET('https://store.steampowered.com/api/appdetails?appids='+str(id))
-        if (response.status_code == 404): # not found
+        response = GET(
+            "https://store.steampowered.com/api/appdetails?appids=" + str(id)
+        )
+        if response.status_code == 404:  # not found
             continue
 
         result = response.json()
 
         try:
-            if (not result[str(id)]['success']):
+            if not result[str(id)]["success"]:
                 raise Exception("Request unsuccessful")
         except:
-            print('Error')
+            print("Error")
             return None
 
         # ensure game is multiplayer
-        for category in result[str(id)]['data']['categories']:
-            if (category['description'].lower() == 'multi-player'):
+        for category in result[str(id)]["data"]["categories"]:
+            if category["description"].lower() == "multi-player":
                 multiplayer = False
                 break
 
-        if (not multiplayer): # requirements is solo, or multi has been found
+        if not multiplayer:  # requirements is solo, or multi has been found
             break
 
         games.remove(id)
-        if (games == []): # no shared multiplayer games
-            print('null')
+        if games == []:  # no shared multiplayer games
+            print("null")
             return None
 
     # print(result[str(id)]['data'])
-    return result[str(id)]['data']
+    return result[str(id)]["data"]
 
 
 def isValidUser(id):
-    response = GET('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key='+key+'&steamids='+id+'&format=json')
-    if (response.status_code != 200):
+    response = GET(f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={key}&steamids={id}&format=json")
+    if response.status_code != 200:
         return False
 
     result = response.json()
-    if (len(result['response']['players']) <= 0):
+    if len(result["response"]["players"]) <= 0:
         return False
 
-    return result['response']['players'][0]['personaname']
+    return result["response"]["players"][0]["personaname"]
