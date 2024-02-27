1e63188d6fc1101a53eb4b1f23efb880775e1eb9
Razzula <jack02gillespie@gmail.com>
2024-02-27 19:03:08 +0100

    Re-implement basic functionality of Steam API

--- src/cogs/SteamCog.py
+++ src/cogs/SteamCog.py
@@ -2,15 +2,18 @@
 """TODO ..."""
 from typing import Final, Any
 from discord.ext import commands
-import apis.steamAPI
+from discord import Embed, Color
+
+from apis.steamAPI import SteamAPI
 
 from BotUtils import BotUtils, Emotes, Emote
+from DatabaseHandler import DatabaseHandler
 from cogs.CogTemplate import CustomCog
 
 class SteamCog(CustomCog):
     """TODO"""
 
-    def __init__(self, bot: commands.Bot, botUtils: BotUtils):
+    def __init__(self, bot: commands.Bot, botUtils: BotUtils, dbHandler: DatabaseHandler, steamAPIKey: str):
         super().__init__('SteamCog', [
             ['decide',      Emotes.STEAM.value.emote],
             ['steam `id`',  Emotes.STEAM_BLACK.value.emote],
@@ -18,6 +21,23 @@ class SteamCog(CustomCog):
 
         self.BOT: Final[commands.Bot] = bot
         self.BOT_UTILS: Final[BotUtils] = botUtils
+
+        self.STEAM_API: Final[SteamAPI] = SteamAPI(steamAPIKey)
+
+        self.DB_HANDLER: Final[DatabaseHandler] = dbHandler
+        self.setupDatabase()
+
+    def setupDatabase(self):
+        """TODO"""
+
+        cursor = self.DB_HANDLER.getCursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS steam_ids (
+                discord_id VARCHAR(18) PRIMARY KEY,
+                steam_id VARCHAR(17)
+            )
+        ''')
+        self.DB_HANDLER.commit()
 
     @commands.command(name='game', pass_context=True)
     async def decideGame(self, context: Any):
@@ -34,8 +54,49 @@ class SteamCog(CustomCog):
                 activeUsers.append(str(user.id))
 
         # get steam ids of users
-        steamIds: list[str] = []
-        for user in activeUsers:
-            # if no steam ID found (TODO)
-            await context.channel.send(f"Sorry, I couldn't find a valid Steam ID for <@{user}>.\nPlease use `!steam <your_steam_id>` to resolve this.")
+        temp = self.DB_HANDLER.arrayToSqlInArgument(activeUsers)
+        res = self.DB_HANDLER.executeOneshot(f'SELECT discord_id, steam_id FROM steam_ids WHERE discord_id IN {temp}')
+
+        if (len(res) < len(activeUsers)):
+            # not all users have a steam id
+            await context.channel.send(f"Sorry, I couldn't find a valid Steam ID for <@???>.\nPlease use `!steam <your_steam_id>` to resolve this.")
             return
+
+        # get shared game library
+        # TODO caching
+        sharedGameLibrary, invalidUsers = self.STEAM_API.getSharedLibrary(res) # TODO: handle invalidUsers
+
+        # select game
+        if (sharedGameLibrary == [None]): # TODO: handle this properly
+            await context.channel.send("No valid Steam users found. Use `!steam <your_steam_id>` to resolve this.")
+
+        elif (not sharedGameLibrary): # TODO: handle this properly
+            await context.channel.send("No shared games found.")
+
+        else:
+            gameEmbed = self.getGameEmbed(
+                sharedGameLibrary, multiplayer=(len(res) > 1) # game must be multi-player if multiple users
+            )
+            await context.channel.send(embed=gameEmbed)
+            # TODO: handle voting
+
+    def getGameEmbed(self, gamesList: list[Any], multiplayer: bool) -> Embed:
+        """TODO"""
+        # select game
+        selectedGame = self.STEAM_API.selectGame(gamesList, requireMultiplayer=multiplayer)
+
+        # generate embed
+        embed = Embed(
+            title=selectedGame.get('name'),
+            color=Color.dark_blue(),
+            description=selectedGame.get('short_description'),
+        )
+        embed.url = f'https://store.steampowered.com/app/{selectedGame["steam_appid"]}'
+        embed.set_thumbnail(url=selectedGame['header_image'])
+
+        if (multiplayer):
+            embed.add_field(name='Multiplayer ✅', value='')
+            embed.add_field(name='Cooperative ❓', value='') # TODO
+        embed.add_field(name='Controller Support ❓', value='') # TODO
+
+        return embed
