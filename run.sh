1fc4cac136ac8699ae63001feaaddbd2b20a7e58
Razzula <jack02gillespie@gmail.com>
2024-03-28 15:12:56 +0000

    Update run.sh script

--- run.sh
+++ run.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-cd "~/jerry-bot" || exit
+# cd "~/jerry-bot" || exit
 
-git fetch origin
+# git fetch origin
 
-if git rev-list --count --left-only "@{u}"...HEAD > /dev/null; then
+# if git rev-list --count --left-only "@{u}"...HEAD > /dev/null; then
 
-    echo "New changes detected. Pulling the latest changes..."
+#     echo "New changes detected. Pulling the latest changes..."
 
-    pip_install_needed=false
+#     pip_install_needed=false
 
-    file_to_check="requirements.txt"
-    if git diff --quiet origin/master..HEAD -- "$file_to_check"; then
-        echo "File $file_to_check is already up-to-date. No dependencies to install."
-    else
-        pip_install_needed=true
-    fi
+#     file_to_check="requirements.txt"
+#     if git diff --quiet origin/master..HEAD -- "$file_to_check"; then
+#         echo "File $file_to_check is already up-to-date. No dependencies to install."
+#     else
+#         pip_install_needed=true
+#     fi
 
-    git pull origin master
-    echo "Successfully pulled the latest changes."
+#     git pull origin master
+#     echo "Successfully pulled the latest changes."
 
-    if [ "$pip_install_needed" = true ]; then
-        pip install -r requirements.txt
-        echo "Successfully installed the latest dependencies."
-    fi
-else
-    echo "Already up-to-date. No new changes to pull."
-fi
+#     if [ "$pip_install_needed" = true ]; then
+#         pip install -r requirements.txt
+#         echo "Successfully installed the latest dependencies."
+#     fi
+# else
+#     echo "Already up-to-date. No new changes to pull."
+# fi
 
-echo "Launching bot..."
+# echo "Launching bot..."
 python3 src/runner.py
